0d7eb3399dc54cf1e4b554121046676e5ea3fdc1
RHS-Rabbi <rabbihossain187@gmail.com>
2026-04-10 20:00:21 +0600

    variable-typeCasting-dataType done

--- variable-typeCasting-dataType/index.php
+++ variable-typeCasting-dataType/index.php
@@ -1,1 +1,18 @@
-<?php  ?>
+<?php
+
+$name = 'Nayemur Rahman'; // string
+$age = 25; // int
+$price = 99.99; // float
+$active = true; // bool
+$data = null; // null
+$tags = ['php', 'laravel']; // array
+
+// type casting
+(int) '42';   // 42
+(float) '3.14';  // 3.14
+(string) 100; // '100'
+(bool) 0; // false
+(array) "Hello";  // ["Hello"]
+
+
+?>
