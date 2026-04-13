6675b43b03da1481a0bf70b047f40caa8c50e68d
RHS-Rabbi <rabbihossain187@gmail.com>
2026-04-13 20:59:36 +0600

    Classes And Object done

---
+++ module-3.1/01-class-and-object/index.php
@@ -0,0 +1,24 @@
+<?php
+// 01 --- Classes And Object
+// Class definition
+
+class BlogPost{
+    public string $title;
+    public string $content;
+    public int $views = 0;
+    public function getInfo(): string{
+        return "{$this->title} ({$this->views} views)";
+    }
+}
+
+// object make (Instantiation)
+
+$post = new BlogPost();
+$post->title = "PHP OOP Learn";
+$post->content = "Detailed....";
+$post->views = 250;
+
+echo $post->getInfo();
+
+
+?>
